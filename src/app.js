41c1416bb4da1a81d993f897bad6e5214a62aded
christiantekle <christiantekle@users.noreply.github.com>
2020-06-07 04:28:21 +0400

    app.js updated

---
+++ src/app.js
@@ -0,0 +1,52 @@
+
+const users = [
+    {
+        id: 1,
+        name: 'Max',
+        orders: {
+            orderId: 101,
+            productId: 201
+        }
+    },
+    {
+        id: 2,
+        name: 'Mark',
+        orders: {
+            orderId: 102,
+            productId: 202
+        }
+    },
+    {
+        id: 3,
+        name: 'John',
+        orders: {
+            orderId: 103,
+            productId: 203
+        }
+    }
+];
+
+const product = [
+    {
+        productId: 101,
+        productName: 'phone',
+        stockAmount: 7000
+    },
+    {
+        productId: 102,
+        productName: 'laptop',
+        stockAmount: 5000
+    },
+    {
+        productId: 103,
+        productName: 'watch',
+        stockAmount: 9000
+    },
+    {
+        productId: 104,
+        productName: 'TV',
+        stockAmount: 1000
+    },
+
+
+];
